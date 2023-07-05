16454ce1abe3557bcd7335d64fc6379152d2c2e0
marcosfuentes94 <marcoslionel@hotmail.it>
2023-07-05 14:53:06 +0200

    esercizio e bonus completati

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-const app = Vue.createApp({
-    data() {
-      return {
-        emailList: [], // ARRAY PER MEMORIZZARE GLI INDIRIZZI EMAIL
-      };
-    },
-  });
+Vue.createApp({
+data() {
+return {
+emailList: [], // ARRAY PER MEMORIZZARE GLI INDIRIZZI EMAIL
+};
+},
+mounted() {
+this.generateEmails(); // METODO CHIAMATO QUANDO IL COMPONENTE VIENE MONTATO
+},
+methods: {
+generateEmails() {
+const apiUrl = 'https://flynn.boolean.careers/exercises/api/random/mail';
+
+const emailPromises = []; // ARRAY PER MEMORIZZARE LE PROMESSE DELLE CHIAMATE API
+
+for (let i = 0; i < 10; i++) {
+    emailPromises.push(axios.get(apiUrl)); // GENERA UNA PROMESSA PER OGNI CHIAMATA API
+}
+
+axios
+    .all(emailPromises) // ATTENDIAMO IL COMPLETAMENTO DI TUTTE LE PROMESSE
+    .then(responses => {
+    const emails = responses.map(response => response.data.response); // ESTRAE GLI INDIRIZZI EMAIL DALLE RISPOSTE
+    this.emailList = emails; // ASSEGNA GLI INDIRIZZI EMAIL ALL'ARRAY EMAIL LIST
+    })
+    .catch(error => {
+    console.error('Si è verificato un errore nella chiamata API:', error); // GESTIONE DEGLI ERRORI
+    });
+},
+},
+}).mount('#app');
